0a6f75d8b58e9434e39e290529407efac5bc1e02
"bunsenstraat" <"somejohndoe@email.com">
2022-11-07 22:43:26 +0100

    yoo3232

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * @title Storageb test
- * @dev Store & retrieve value in a variable
+ * @dev Store & retrieve value in a va333riable
  */
 contract Storage {
 
