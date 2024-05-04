4eba6bd1818e7329c8a77bc66417a14cf6b7bb31
bunsenstraat3 <filip@give.it>
2024-05-04 18:01:25 +0200

    revert

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /** 
  * @title Ballot
- * @dev Implements voting process along with vote delegation some comment
+ * @dev Implements voting process along with vote delegation
  */
 contract Ballot {
    
